75cef625fa553119034ad139e31078ab1a6860d4
vgabrielk <vfelix1553@gmail.com>
2025-05-18 18:29:43 -0300

    redirect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "./globals.css";
 import { Playfair_Display, Poppins, Mulish } from 'next/font/google';
 
@@ -53,6 +54,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="pt-BR" className={`${poppins.className} ${playfairDisplay.className} ${mulish.className}`}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="antialiased">
         {children}
       </body>
